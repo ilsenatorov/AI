4f209b7761a8869b7b5b49544ab12548ff58d33e
JeffSolo <jeffreycsolomon@gmail.com>
2019-05-09 21:03:23 +0200

    actually uploaded cnf code this time

--- Practical1/Practical_Sheet_1_Scripts/cnf_transformation.py
+++ Practical1/Practical_Sheet_1_Scripts/cnf_transformation.py
@@ -2,79 +2,83 @@
 # -*- coding: utf-8 -*-
 from formula import Atom, Not
 
-    
+
 def eliminate_iff(f):
     """ Eliminates the '↔' operator and returns the given formula transformed.
     """
-    # ToDo
-    pass
-    
+    try:
+        return (f.lchild >> f.rchild) & (f.rchild >> f.lchild)
+    except AttributeError as e:
+        print(e)
+
 def eliminate_implies(f):
     """ Eliminates the '→' operator and returns the given formula transformed.
     """
-    # ToDo
-    pass
-    
-    
+    try:
+        return ~f.lchild | f.rchild
+    except AttributeError as e:
+        print(e)
+
 def move_not_inward(f):
     """ Moves the '¬' operator inward and returns the given formula transformed.
     """
-    # f = ¬inside
     inside = f.child
-    
+
     if isinstance(inside, Atom) or isinstance(inside, Not):
-        # ToDo
-        pass
-        
+        return ~inside
+
+    inside.lchild = ~inside.lchild
+    inside.rchild = ~inside.rchild
+
     if inside.op == '∧':
-        # ToDo
-        pass
+        inside.op = '∨'
+
     elif inside.op == '∨':
-        # ToDo
-        pass
-        
+        inside.op = '∧'
+
+    return inside
+
 def distribute_or_over_and(f):
     """ Distributes the or operators over ands and returns the given formula transformed.
     """
-    left = f.lchild 
+    left = f.lchild
     right = f.rchild
-    
+
     left_is_atom = isinstance(left, Atom) or isinstance(left, Not)
     right_is_atom = isinstance(right, Atom) or isinstance(right, Not)
-    
+
     if left_is_atom and right_is_atom:
-        # ToDo
-        pass
+        return f
     elif not left_is_atom and not right_is_atom and left.op != f.op and right.op != f.op:
-        # ToDo
-        pass
+        return ((left.lchild | right.lchild)
+                & (left.rchild | right.lchild)
+                & (left.lchild | right.rchild)
+                & (left.rchild | right.rchild))
     elif left_is_atom and right.op != f.op:
-        # ToDo
-        pass
+        return ((left | right.lchild) & (left | right.rchild))
     elif right_is_atom and left.op != f.op:
-        # ToDo
-        pass
+        return ((right | left.lchild) & (right | left.rchild))
     else:
         return f
 
 def eliminate_operator(f, eliminator, op):
     if isinstance(f, Atom):
         return f
-    
+
     if isinstance(f, Not):
         if op == '¬':
             if isinstance(f.child, Atom) or isinstance(f.child, Not):
                 return f
             f = eliminator(f)
-        else: 
+        else:
             return ~eliminate_operator(f.child, eliminator, op)
-    
+
     if op == f.op:
         f = eliminator(f)
-    
+
     lchild = eliminate_operator(f.lchild, eliminator, op)
     rchild = eliminate_operator(f.rchild, eliminator, op)
-    
+
     if f.op == '∧':
         return lchild & rchild
     elif f.op == '∨':
@@ -83,7 +87,8 @@ def eliminate_operator(f, eliminator, op):
         return lchild >> rchild
     elif f.op == '↔':
         return lchild << rchild
-    
+
+
 
 a = Atom('a')
 b = Atom('b')
@@ -105,6 +110,3 @@ print("Applying Rule 4")
 f = eliminate_operator(f, distribute_or_over_and, '∨')
 f = eliminate_operator(f, distribute_or_over_and, '∨')
 print(f)
-
-
-
